32654b4385eb08c4a9a455237e2a89acd0fc4b87
Pedro Arthur <pdroarthurls@gmail.com>
2018-07-20 16:53:24 -0300

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/udemy/cursoSpringIonic/services/CategoriaService.java
+++ src/main/java/com/udemy/cursoSpringIonic/services/CategoriaService.java
@@ -7,15 +7,22 @@ import org.springframework.stereotype.Service;
 
 import com.udemy.cursoSpringIonic.domain.Categoria;
 import com.udemy.cursoSpringIonic.repositories.CategoriaRepository;
+import com.udemy.cursoSpringIonic.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
-	
+
 	@Autowired
 	private CategoriaRepository repo;
-	
-	public Categoria buscar(Integer id) {
+
+	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
+
+//	public Categoria buscar(Integer id) {
+//		Optional<Categoria> obj = repo.findById(id);
+//		return obj.orElse(null);
+//	}
 }
