9a8e2ac28091d66530e8d2b20e362b337f27a4b5
Pedro Arthur <pdroarthurls@gmail.com>
2018-07-19 21:02:16 -0300

    Criando operação de instanciação

--- src/main/java/com/udemy/cursoSpringIonic/DemoApplication.java
+++ src/main/java/com/udemy/cursoSpringIonic/DemoApplication.java
@@ -1,12 +1,30 @@
 package com.udemy.cursoSpringIonic;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class DemoApplication {
+import com.udemy.cursoSpringIonic.domain.Categoria;
+import com.udemy.cursoSpringIonic.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class DemoApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null,"Informática");
+		Categoria cat2 = new Categoria(null,"Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
 	}
 }
