47032fc4fb754657dcf420c929b9ba9a92b6e7b4
Unknown <pdroarthurls@gmail.com>
2018-08-25 20:15:24 -0300

    Validação sintática com Bean validation

--- src/main/java/com/udemy/cursoSpringIonic/services/CategoriaService.java
+++ src/main/java/com/udemy/cursoSpringIonic/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.udemy.cursoSpringIonic.domain.Categoria;
+import com.udemy.cursoSpringIonic.dto.CategoriaDTO;
 import com.udemy.cursoSpringIonic.repositories.CategoriaRepository;
 import com.udemy.cursoSpringIonic.services.exceptions.DataIntegrityException;
 import com.udemy.cursoSpringIonic.services.exceptions.ObjectNotFoundException;
@@ -57,6 +58,11 @@ public class CategoriaService {
 				orderBy);
 		return repo.findAll(pageRequest);
 	}
+	
+	public Categoria fromDTO(CategoriaDTO objDTO) {
+		return new Categoria(objDTO.getId(),objDTO.getNome());
+	}
+	
 //	public Categoria buscar(Integer id) {
 //		Optional<Categoria> obj = repo.findById(id);
 //		return obj.orElse(null);
