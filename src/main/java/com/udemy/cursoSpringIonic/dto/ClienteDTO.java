32467e4b8ac66ff3c76ddc24465e6de385ab728f
Pedro Arthur <pdroarthurls@gmail.com>
2018-12-20 16:24:03 -0200

    Validacao customizada: email nao repetido na atualizaco de Cliente

--- src/main/java/com/udemy/cursoSpringIonic/dto/ClienteDTO.java
+++ src/main/java/com/udemy/cursoSpringIonic/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.udemy.cursoSpringIonic.domain.Cliente;
+import com.udemy.cursoSpringIonic.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
