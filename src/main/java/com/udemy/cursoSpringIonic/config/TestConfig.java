d3fac83a232b43094e89ab041f7602ec9cc30720
Pedro Arthur <pdroarthurls@gmail.com>
2018-12-21 19:18:23 -0200

    Criando o profile de teste

--- src/main/java/com/udemy/cursoSpringIonic/config/TestConfig.java
+++ src/main/java/com/udemy/cursoSpringIonic/config/TestConfig.java
@@ -1,32 +1,30 @@
 package com.udemy.cursoSpringIonic.config;
-//package com.udemy.cursoSpringIonic.config;
-//
-//import java.text.ParseException;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.context.annotation.Profile;
-//
-//import com.nelioalves.cursomc.services.DBService;
-//import com.nelioalves.cursomc.services.EmailService;
-//import com.nelioalves.cursomc.services.MockEmailService;
-//
-//@Configuration
-//@Profile("test")
-//public class TestConfig {
-//
-//	@Autowired
-//	private DBService dbService;
-//	
-//	@Bean
-//	public boolean instantiateDatabase() throws ParseException {
-//		dbService.instantiateTestDatabase();
-//		return true;
-//	}
-//	
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.udemy.cursoSpringIonic.services.DBService;
+
+
+@Configuration
+@Profile("test")
+public class TestConfig {
+
+	@Autowired
+	private DBService dbService;
+	
+	@Bean
+	public boolean instantiateDatabase() throws ParseException {
+		dbService.instantiateTestDatabase();
+		return true;
+	}
+	
 //	@Bean
 //	public EmailService emailService() {
 //		return new MockEmailService();
 //	}
-//}
+}
